6d17de4a9b8716fda59ca9aa77ec38c97b0e24c9
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-11 18:45:22 +0200

    Rename endpoint

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main(){
 	v1Router := chi.NewRouter()
 
 	v1Router.Get("/healthz", handlerReadiness)
-	v1Router.Get("/error", handlerErr)
+	v1Router.Get("/err", handlerErr)
 
 	router.Mount("/v1", v1Router)
 
