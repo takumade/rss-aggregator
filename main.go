76bc5bd00c6954a11f6eb663180817d43f33786b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-11 18:00:21 +0200

    Fic env bug

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
-package main;
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 
 func main(){
 	fmt.Println("Hello world")
+
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+ 	}
+
+	fmt.Println("PORT: ", portString)
+
+
+
+
 }
