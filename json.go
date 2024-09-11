aec912871ba3151e39f21e2780dd32a0cba2ef3f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-11 18:41:06 +0200

    Add respond with error

--- json.go
+++ json.go
@@ -6,6 +6,21 @@ import (
 	"encoding/json"
 )
 
+func responseWithError(w http.ResponseWriter, code int, msg string){
+	if code > 499 {
+		log.Println("Responding with 5XX error: ", msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
 	dat, err := json.Marshal(payload)
 
