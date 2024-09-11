b34cdb7d107cd632a2ff700c7dac974184f0fc9f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-11 18:23:34 +0200

    Add cors rules

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -20,6 +21,16 @@ func main(){
  	}
 
 	router := chi.NewRouter()
+
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET","POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
 
 	srv := &http.Server{
 		Handler: router,
