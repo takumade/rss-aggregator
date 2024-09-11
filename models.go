93eeece979701f35cc614c98b4f047d24667d831
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-11 22:35:40 +0200

    Add model

---
+++ models.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rss-aggregator/internal/database"
+)
+
+type User struct {
+	ID   uuid.UUID  `json:"id"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	Name   string  `json:"name"`
+}
+
+
+func databaseUsertoUser(dbUser database.User) User {
+     return User{
+		ID: dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name: dbUser.Name,
+	 }
+
+}
