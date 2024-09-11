4e76435b54c8d027cf26a72444ee3c99295d0699
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-11 22:25:49 +0200

    modify endpoint

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main(){
 
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
-	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Post("/user", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
