c3a9c4399afd76aa115da901bbd080bc663f07b1
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-16 21:55:40 +0200

    iMP[ORT AUTH PACKAGE

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/rss-aggregator/internal/auth"
 	"github.com/rss-aggregator/internal/database"
 )
 
@@ -38,5 +39,26 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return 
 	}
 	
-	respondWithJSON(w, 200, databaseUsertoUser(user))
+	respondWithJSON(w, 201, databaseUsertoUser(user))
+}
+
+
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request){
+	apiKey, err := auth.GetAPIKey(r.Header)
+
+	if err != nil {
+		responseWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Couldnt get user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUsertoUser((user)))
+
 }
