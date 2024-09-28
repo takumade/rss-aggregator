7a495eea165a86e7159d88d8db0a847847dc6f6b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-28 22:20:22 +0200

    Modify fields for feeds

--- sql/schema/003_feeds.sql
+++ sql/schema/003_feeds.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 CREATE TABLE feeds (
     id UUID PRIMARY KEY,
-    created TIMESTAMP NOT NULL,
-    updated TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL,
     url TEXT UNIQUE NOT NULL,
     user_id UUID REFERENCES users(id) ON DELETE CASCADE
