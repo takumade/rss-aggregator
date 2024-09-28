558a4d18b84d7c6a0923622e283cdb4b1850c72e
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-28 23:49:14 +0200

    Add a feed follow model and function

--- models.go
+++ models.go
@@ -25,6 +25,14 @@ type Feed struct {
 	UserID uuid.UUID `json:"user_id"`
 }
 
+type FeedFollow struct {
+	ID   uuid.UUID  `json:"id"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	UserID uuid.UUID `json:"user_id"`
+	FeedID uuid.UUID `json:"feed_id"`
+}
+
 func databaseUsertoUser(dbUser database.User) User {
      return User{
 		ID: dbUser.ID,
@@ -57,4 +65,14 @@ func databaseFeedstoFeeds(dbFeed []database.Feed) []Feed {
 	}
 
 	return feeds
+}
+
+func databaseFeedFollowtoFeedFollow(dbFeedFollow database.FeedFollow) FeedFollow {
+	return FeedFollow{
+		ID: dbFeedFollow.ID,
+		CreatedAt: dbFeedFollow.CreatedAt,
+		UpdatedAt: dbFeedFollow.UpdatedAt,
+		UserID: dbFeedFollow.UserID,
+		FeedID: dbFeedFollow.FeedID,
+	}
 }
