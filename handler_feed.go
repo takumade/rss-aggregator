056ae5e31646e0d29009bfeecc9dd2ebec374228
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-28 23:01:01 +0200

    Handle create feed

---
+++ handler_feed.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rss-aggregator/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request){
+	
+	type parameters struct {
+		Name string `json: "name"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return 
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: params.Name,
+	})
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Coudnt create user: %v", err))
+		return 
+	}
+	
+	respondWithJSON(w, 201, databaseUsertoUser(user))
+}
+
+
