d9746fd2d21c38fa81fce86e463e943349dfc127
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-28 23:50:44 +0200

    Add feed follow endpoint

--- main.go
+++ main.go
@@ -66,6 +66,7 @@ func main(){
 	v1Router.Get("/user", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+	v1Router.Post("/feed-follow", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
 
 	router.Mount("/v1", v1Router)
 
