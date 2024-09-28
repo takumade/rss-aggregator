6c978c72f6ff9cd89053da4f5430f9f60462c651
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-28 22:26:32 +0200

    fIx import errors

--- middleware_auth.go
+++ middleware_auth.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-
+	"github.com/rss-aggregator/internal/auth"
 	"github.com/rss-aggregator/internal/database"
 )
 
@@ -10,8 +11,22 @@ import (
 type authedHandler func(http.ResponseWriter, *http.Request, database.User)
 
 
-func (cfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
+func (apiCfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		
+		apiKey, err := auth.GetAPIKey(r.Header)
+
+	if err != nil {
+		responseWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Couldnt get user: %v", err))
+		return
+	}
+
+	handler(w, r, user)
 	}
 }
