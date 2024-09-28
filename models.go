63af779067876a48a532dc914e660c00c980cd03
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-28 23:12:02 +0200

    Add feed model

--- models.go
+++ models.go
@@ -16,6 +16,15 @@ type User struct {
 }
 
 
+type Feed struct {
+	ID   uuid.UUID  `json:"id"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	Name   string  `json:"name"`
+	Url  string `json:"url"`
+	UserID uuid.UUID `json:"user_id"`
+}
+
 func databaseUsertoUser(dbUser database.User) User {
      return User{
 		ID: dbUser.ID,
@@ -25,4 +34,16 @@ func databaseUsertoUser(dbUser database.User) User {
 		APIKey: dbUser.ApiKey,
 	 }
 
+}
+
+
+func databaseFeedtoFeed(dbFeed database.Feed) Feed {
+	return Feed{
+		ID: dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name: dbFeed.Name,
+		Url: dbFeed.Url,
+		UserID: dbFeed.UserID,
+	}
 }
