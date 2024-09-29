6903a31c73afe409c0180d14ecf731609c988d8c
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-29 22:10:44 +0200

    Add handlerGetPostsForUser in user handler

--- handler_user.go
+++ handler_user.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -44,6 +45,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 
 
 func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User){
+	respondWithJSON(w, 200, databaseUsertoUser((user)))
+
+}
+
+
+func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User){
+
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit: 10,
+	})
+
+	if err != nil {
+		log.Printf("Error getting posts for user: %v", err)
+	}
+
 	respondWithJSON(w, 200, databaseUsertoUser((user)))
 
 }
