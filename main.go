a57c92b09a8dc730df922a8def45de3c98c7dc2b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-29 20:55:31 +0200

    Call start scrapping functrion

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -42,10 +43,13 @@ func main(){
 	}
 
 
-
+    db := database.New(conn)
 	apiCfg := apiConfig{
-		DB:database.New(conn),
+		DB:db,
 	}
+
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
