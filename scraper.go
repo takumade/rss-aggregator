ce8e7c9790c7e7b4362b281f5786bcd9894fe9d8
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-29 21:25:53 +0200

    Save  posts

--- scraper.go
+++ scraper.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/rss-aggregator/internal/database"
 )
 
@@ -62,6 +64,36 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed){
 
 	for _, item := range rssFeed.Channel.Item {
 		log.Println("Found Post: ", item.Title, "on feed: ", feed.Name)
+		description := sql.NullString{}
+
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+
+		if err != nil {
+			log.Println("Error parsing date %v with err %v ", item.PubDate, err)
+			continue
+		}
+
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID: uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Title: item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url: item.Link,
+			FeedID: feed.ID,
+		})
+
+		if err != nil {
+			log.Println("Error creating post: ", err)
+		}
+		
 	}
 
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
